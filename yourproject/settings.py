686e54e0a607c1ebe48332f425270ad8ca9bd0aa
DominikSabat <60387429+DominikSabat@users.noreply.github.com>
2021-08-11 21:27:55 +0200

    change

--- yourproject/settings.py
+++ yourproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yourproject',
         'USER': 'postgres',
-        'PASSWORD': '57oPsDJXfp',
+        'PASSWORD': '---------',
         'HOST': 'localhost',
         'PORT': '5432',
     }
